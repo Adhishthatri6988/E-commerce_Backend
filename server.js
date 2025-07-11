161c68c019bf89ca2a8277f0e123271a6d700a67
Adhishthatri <asunasingh2003@gmail.com>
2025-07-11 23:07:00 +0530

    database added and server.js added

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
-import express from express;
-import cors from cors ;
-import 'dotenv/config'
+import express from 'express';
+import cors from 'cors' ;
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+
+// App config
+const app = express() 
+const port = process.env.PORT || 4000 
+
+connectDB()
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+//API endpoints
+app.get('/' , (req , res) => {
+  res.send("API working");
+})
+
+app.listen(port , () => console.log('Server statred on PORT : ' + port))
 
