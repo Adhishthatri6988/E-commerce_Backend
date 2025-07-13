ac180c9ad835b8eeb09afa1be9c92b93f193c9a0
Adhishthatri <asunasingh2003@gmail.com>
2025-07-13 22:58:53 +0530

    multer added for images

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,17 +1,25 @@
 // function for add product
 
-const addProduct = async (req, res) => {};
+const addProduct = async (req, res) => {
+  
+};
 
 // function for list product
 
-const listProduct = async (req, res) => {};
+const listProduct = async (req, res) => {
+
+};
 
 // function for remove product
 
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+
+};
 
 // function for single product info
 
-const singleProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {
+
+};
 
 export { addProduct, listProduct, removeProduct, singleProduct };
