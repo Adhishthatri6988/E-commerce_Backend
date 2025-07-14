57a28e9185933b9508f92453971cf727cd1e30ea
Adhishthatri <asunasingh2003@gmail.com>
2025-07-14 22:10:57 +0530

    admin login user controller added

--- controllers/productController.js
+++ controllers/productController.js
@@ -61,8 +61,8 @@ const listProduct = async (req, res) => {
 
 const removeProduct = async (req, res) => {
     try {
-      await productModel.findByIdAndDelete(req.body.id);
-      res.json({success:true , message:"Product removed"});
+      await productModel.findByIdAndDelete(req.body.id)
+      res.json({success:true , message:"Product removed"})
     } catch (error) {
       console.log(error)
       res.json({success:false, message: error.message})
@@ -75,7 +75,7 @@ const singleProduct = async (req, res) => {
   try {
       const {productId} = req.body
       const product = await productModel.findById(productId)
-      res.json({success:true ,product})
+      res.json({success:true ,product});
   } catch (error) {
     console.log(error)
       res.json({success:false, message: error.message})
