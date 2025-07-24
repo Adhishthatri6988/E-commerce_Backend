91cb1693e6cd12b617560e5fac6dab674e706545
Adhishthatri <asunasingh2003@gmail.com>
2025-07-24 17:10:07 +0530

    bug fix

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,23 +1,12 @@
-import fs from 'fs';
-import path from 'path';
-import multer from 'multer';
+import multer from 'multer'
 
-// Ensure uploads folder exists
-const uploadDir = path.join(process.cwd(), 'uploads');
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-// Setup multer storage
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadDir); // use verified directory
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
+  filename:function(req , file, callback){
+    callback(null , file.originalname)
   }
-});
+})
 
-const upload = multer({ storage });
 
-export default upload;
+const upload = multer({storage})
+
+export default upload
