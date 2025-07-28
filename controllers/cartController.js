eed21bd0e3f8cd7b5f94d4c448c512a932cf56a7
Adhishthatri <asunasingh2003@gmail.com>
2025-07-28 23:45:15 +0530

    auth func added a d add to cart added

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,8 +1,35 @@
+import userModel from "../models/userModel";
 
 
 // ADD PRODUCTS TO USER CARTS
 const addToCart = async (req, res) => {
+  try {
+    const { userId, itemId, size } = req.body;
+    
+    const userData = await userModel.findById(userId);
 
+    let cartData = await userData.cartData;
+
+    if(cartData[itemId]){
+      if(cartData[itemId][size]){
+        cartData[itemId][size] += 1; // Increment quantity if item already exists
+      }else{
+        cartData[itemId][size] = 1; // Add new size with quantity 1
+      }
+    }else{
+      cartData[itemId] = {};
+      cartData[itemId][size]= 1 // Add new item with size and quantity 1
+    }
+
+
+    await userModel.findByIdAndUpdate(userId, { cartData }, { new: true });
+    res.json({ success: true, message: 'Item added to cart successfully', cartData });
+
+
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: 'Error adding item to cart', error: error.message });
+  }
 }
 
 
---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+const authUser = async (req, res, next) => {
+  const {token} = req.headers;
+
+  if (!token) {
+    return res.status(401).json({message: 'No token provided, authorization denied'});
+  }
+
+  try {
+    const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+    req.body.userId = token_decode.id;
+    next();
+  } catch (error) {
+    console.log(error)
+    return res.json({success:false,message: 'Token is not valid'});
+  }
+}
+
+export default authUser;
