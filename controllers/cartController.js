73968d2a209670d8b389e7bb945329eeb8e124e3
Adhishthatri <asunasingh2003@gmail.com>
2025-07-28 23:28:26 +0530

    added cartroute and cartcontroller

---
+++ controllers/cartController.js
@@ -0,0 +1,20 @@
+
+
+// ADD PRODUCTS TO USER CARTS
+const addToCart = async (req, res) => {
+
+}
+
+
+//update products in user carts
+const updateCart = async (req, res) => {
+  
+}
+
+//get user carts
+const getUserCart = async (req, res) => {
+  
+}
+
+
+export { addToCart, updateCart, getUserCart };
