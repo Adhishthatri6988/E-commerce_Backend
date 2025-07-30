8dc3f31fe80ed48e3700c31b07fd155ea10f0034
Adhishthatri <asunasingh2003@gmail.com>
2025-07-30 20:27:29 +0530

    cartController done

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -35,12 +35,38 @@ const addToCart = async (req, res) => {
 
 //update products in user carts
 const updateCart = async (req, res) => {
-  
+  try {
+      const { userId, itemId, size, quantity } = req.body;
+      const userData = await userModel.findById(userId);
+
+      let cartData = await userData.cartData;
+      
+      cartData[itemId][size] = quantity; // Update quantity for the size
+      
+      await userModel.findByIdAndUpdate(userId, { cartData }, { new: true });
+      
+    res.json({ success: true, message: 'Cart updated successfully', cartData });
+
+  } catch (error) {
+     console.log(error)
+    res.json({ success: false, message: 'Error adding item to cart', error: error.message });
+  }
 }
 
 //get user carts
 const getUserCart = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const userData = await userModel.findById(userId);
+    let cartData = await userData.cartData;
+
+    res.json({ success: true, message: 'Cart retrieved successfully', cartData });
+
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: 'Error adding item to cart', error: error.message });
   
+  }
 }
 
 
