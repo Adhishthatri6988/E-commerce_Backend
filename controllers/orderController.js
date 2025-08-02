a41a38e94afbbc945b3bd3fb4e524ffa595d7135
Adhishthatri <asunasingh2003@gmail.com>
2025-08-02 19:35:53 +0530

    controller fun added

---
+++ controllers/orderController.js
@@ -0,0 +1,40 @@
+
+
+//PLACING ORDER using COD Method
+
+const placeOrder = async(req , res) => {
+
+}
+
+
+//PLACING ORDER using STRIPE Method
+
+const placeOrderStripe = async(req , res) => {
+  
+}  
+
+
+//PLACING ORDER using RazorPay Method
+
+const placeOrderRazorpay = async(req , res) => {
+  
+}
+
+//All Orders for a admin panel
+const allOrders = async(req, res) => {
+
+}
+
+
+//User order data for frontend
+const userOrders = async(req, res) => {
+
+}
+
+
+//update order status for admin panel
+const updateStatus = async(req , res) => {
+
+}
+
+export {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus};
