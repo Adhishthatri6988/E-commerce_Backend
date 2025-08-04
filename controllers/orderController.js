f7bc5a55e4cd15b3cc63687fa290b6129555e622
Adhishthatri <asunasingh2003@gmail.com>
2025-08-04 22:58:47 +0530

    stripe for payment added

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,4 +1,20 @@
+import { currency } from '../../admin/src/App.jsx';
 import orderModel from '../models/orderModel.js';
+import userModel from '../models/userModel.js';
+import Stripe from 'stripe';
+
+//global variables
+const currency =  'USD';
+const deliveryCharges = 10;
+
+
+//gateway initialization
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+
+
+
+
 
 //PLACING ORDER using COD Method
 
@@ -40,7 +56,65 @@ const placeOrder = async(req , res) => {
 //PLACING ORDER using STRIPE Method
 
 const placeOrderStripe = async(req , res) => {
+  try {
+    const { userId , amount , items , address} = req.body;
+    const {origin} = req.headers;
+    const orderData = {
+      userId,
+      amount,
+      items,
+      address,
+      paymentMethod:"Stripe",
+      payment:false,
+      date: Date.now(),
+    }
+
+     const newOrder = new orderModel(orderData);
+    await newOrder.save();
+
+    const line_items = items.map((item) => ({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: item.name
+        },
+        unit_amount: item.price * 100, // Convert to cents
+      },
+      quantity: item.quantity,
+    }))
+
+    line_items.push({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: "Delivery Charges",
+        },
+        unit_amount: deliveryCharges, // Example shipping cost
+      },
+      quantity: 1,
+    })
+
+    const session = await stripe.checkout.sessions.create({
+      
+      success_url: `${origin}/verify?succes=true&orderId=${newOrder._id}`,
+      cancel_url: `${origin}/verify?succes=false&orderId=${newOrder._id}`,
+      line_items,
+      mode: 'payment',
   
+    });
+    
+    res.json({
+      success: true,
+      session_url: session.url,
+    })
+
+  } catch (error) {
+     console.log( error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
 }  
 
 
